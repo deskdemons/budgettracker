145deaee883259bfce5c403858b3f70366687cf9
Saurav Adhikari <ersauravadhikari@gmail.com>
2021-08-01 20:08:07 +0545

    Formatting and VSCode file ignored

--- main.cpp
+++ main.cpp
@@ -7,16 +7,14 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-
     Budget b1("1,Rajeev ko Tshirt,Clothing,12th Jan 2021-12:17:23 PM,NRP,-1300");
     b1.display_information();
 
     BudgetManager bdb;
-    for (int i =0; i < bdb.all().size(); i++) {
+    for (int i = 0; i < bdb.all().size(); i++)
+    {
         Budget object = bdb.all()[i];
         object.display_information();
     }
-
     return 0;
 }
