05fd8b2abbe8549612c9a7983bb5257c6fdc2b9c
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2021-08-24 13:32:11 +0545

    Forex page done (semi)

--- pages/forex_page/forex_page.h
+++ pages/forex_page/forex_page.h
@@ -8,24 +8,32 @@
 #include "../../gui/banner/banner.h"
 #include "../../gui/topbar/topbar.h"
 #include "../../gui/dropdown_menu/dropdownMenu.h"
+#include "../../gui/table/table.h"
 
 class ForexPage{
+    Table table;
     Topbar topbarForex;
     sf::Text output_1, output_2;
     sf::Text text_amount, text_from, text_to, text_table_title, t_amount_text_box_warning;
+    RoundedRectangle rounded_rectangle;
     sf::Font font;
     Textbox t_amount_text_box;
     DropdownMenu dropdown_currency_list, dropdown_currency_list_2;
 
-    sf::CircleShape currency_change_icon_shape; //circle shape for user profile
-    sf::Texture currency_change_icon_pic; //user profile default photo texture
+    sf::CircleShape currency_change_icon_shape; //circle shape for icon
+    sf::Texture currency_change_icon_pic; //default icon texture
 
+    std::string converted_value_str;
+
+    Button convert_button;
+    double stringToDouble(std::string s);
+    std::string doubleToString(double num);
 public:
     ForexPage();
     void eventHandler(sf::Event &event, sf::RenderWindow &window);
+    void convert_value();
     void drawer();
     void drawTo(sf::RenderWindow &window);
 };
-
 #endif
 
